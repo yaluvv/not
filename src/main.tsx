dd9972a0edf247bfd001510278a25a20df892be4
yaluvv <yaluvv@yandex.ru>
2024-06-08 06:49:11 +0300

    commit 6

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import WebApp from '@twa-dev/sdk'
 import './index.css'
+import '../public/tg.js'
 
 
 WebApp.ready();
@@ -10,8 +11,15 @@ WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <body>
       
     <App />
+
+    <script>
+    window.Telegram.WebApp.isExpanded = true;
+    window.Telegram.WebApp.expend();
+    </script>
+    </body>
    
   </React.StrictMode>,
 )

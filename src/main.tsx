1721606fc8cc42c0d07db390a5328ae39b899b1e
yaluvv <yaluvv@yandex.ru>
2024-06-09 22:37:46 +0300

    added anayitics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/react"
 import WebApp from '@twa-dev/sdk'
 import './index.css'
 
@@ -14,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
     <App />
 
-
+<Analytics/>
   
   </React.StrictMode>
   

ffe3189a168f8c8fe9bd18c59b13a848d9f20f67
yaluvv <yaluvv@yandex.ru>
2024-06-09 22:56:24 +0300

    fix speed

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import WebApp from '@twa-dev/sdk'
 import './index.css'
 
 
 WebApp.ready();
-WebApp.expand()
+WebApp.expand();
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -17,8 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
 
 <Analytics/>
-<SpeedInsights/>
-  
+
   </React.StrictMode>
   
 )
